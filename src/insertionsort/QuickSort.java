927e5545c9603c4dcaf7f6e183b42392b67235f0
suranihiranyada <suranihiranyada@gmail.com>
2016-05-05 22:41:35 +0530

    checking again

---
+++ src/insertionsort/QuickSort.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insertionsort;
+
+/**
+ *
+ * @author suranihiranyada
+ */
+public class QuickSort {
+    public class SelectionSort {
+    public void a(){
+    System.out.println("a");}}
+}
